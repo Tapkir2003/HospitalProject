c1434646d4198f5f67c3bfac9c03bf88effa6f81
prajwaltapkir <165243119+Tapkir2003@users.noreply.github.com>
2025-12-04 14:40:58 +0530

    Add files via upload

---
+++ Hospitallmain.java
@@ -0,0 +1,16 @@
+package Hospitalmain;
+
+import com.Hospital.serviceIMPL.HospitalIMPL;
+
+public class Hospitallmain {
+
+	public static void main(String[] args) {
+		HospitalIMPL hp=new HospitalIMPL();
+		
+		//hp.createPatientAccount();
+		
+		hp.viewDetails();
+
+	}
+
+}
