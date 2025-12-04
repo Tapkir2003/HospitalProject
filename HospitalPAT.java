2235d693d4bcbe266058ba5be9abdb8adc2a6b7e
prajwaltapkir <165243119+Tapkir2003@users.noreply.github.com>
2025-12-04 14:39:21 +0530

    Add files via upload
    contain methods in interface file

---
+++ HospitalPAT.java
@@ -0,0 +1,15 @@
+package com.Hospital.serviceMETH;
+
+public interface HospitalPAT {
+	
+    void createPatientAccount();
+	
+	void viewDetails();
+
+    void updatePatientDetails();
+	
+	
+	
+	void billpayment();
+	
+}
